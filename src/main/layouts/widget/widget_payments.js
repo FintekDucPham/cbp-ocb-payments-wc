9b916fb70982dff12fba87326580455fd3411758
Mariusz Kaczmarczyk <mariusz.kaczmarczyk@asseco.pl>
2015-06-17 18:55:28 +0200

    payments - uruchomienie apki.

--- src/main/layouts/widget/widget_payments.js
+++ src/main/layouts/widget/widget_payments.js
@@ -4,21 +4,6 @@ angular.module('raiffeisen-payments').controller('PaymentsWidgetController', fun
         pageNumber: 1,
         pageSize: 10
     };
-
-	$scope.paymentsPromise = paymentsService.search($scope.params).then(function(paymentList) {
-        $scope.paymentList = paymentList;
-        if(paymentList.content.length > 0) {
-        	angular.element(document.getElementsByClassName("widget-title-" + webComponentRegistry["raiffeisen-payments"].simpleName)).append("<span> (" + paymentList.totalElements + ")</span>");
-        }
-    });
-
-
-	$scope.submit = function(ctx, params, rel) {
-       	return paymentsService.relationSearch(ctx, params, rel).then(function(paymentList) {
-          	return paymentList;
-        });
-    };
-
     $scope.widgetContent = pathService.generateTemplatePath("raiffeisen-payments") + '/layouts/widget/widget_payments_content.html';
 
 });
