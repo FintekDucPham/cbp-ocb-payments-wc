0581b446c3e6705b11f361e27b328a75eab0487a
Grzegorz Gurgul <grzegorz.gurgul@asseco.pl>
2015-07-27 08:19:10 +0200

    podsumowanie kwot

--- src/main/modules/new/fill/zus/payments_new_zus_fill.js
+++ src/main/modules/new/fill/zus/payments_new_zus_fill.js
@@ -1,4 +1,28 @@
 angular.module('raiffeisen-payments')
-    .controller('NewZusPaymentFillController', function ($scope) {
+    .constant('zusPaymentInsurances', ['sp', 'zd', 'fp_fgsp', 'fep'])
+    .controller('NewZusPaymentFillController', function ($scope, lodash, zusPaymentInsurances) {
+
+        $scope.zusInsuranceTypes = zusPaymentInsurances;
+
+        function calculateInsurancesAmount() {
+            return lodash.map(lodash.groupBy(lodash.filter($scope.payment.formData.insurances, function (element) {
+                return element.active && !!element.currency;
+            }), 'currency'), function (values) {
+                var totalAmount = 0;
+                lodash.forEach(values, function (value) {
+                    totalAmount += value.amount;
+                });
+                return {
+                    currency: values[0].currency,
+                    amount: totalAmount
+                };
+            });
+        }
+
+        $scope.totalPaymentAmount = [];
+
+        $scope.$watch('payment.formData.insurances', function () {
+            $scope.totalPaymentAmount = calculateInsurancesAmount();
+        }, true);
 
     });
