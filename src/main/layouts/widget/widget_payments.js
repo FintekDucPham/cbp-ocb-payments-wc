ed4f10deeec12e184bd71a0fb99674c9c42ec4da
Grzegorz Gurgul <grzegorz.gurgul@asseco.pl>
2015-08-12 12:33:01 +0200

    Kafelek płatności

--- src/main/layouts/widget/widget_payments.js
+++ src/main/layouts/widget/widget_payments.js
@@ -1,9 +1,25 @@
-angular.module('raiffeisen-payments').controller('PaymentsWidgetController', function($scope, $rootScope, $location, $timeout,$state, pathService, paymentsService, transactionService) {
+angular.module('raiffeisen-payments').controller('PaymentsWidgetController', function($scope, paymentsSummaryService, $rootScope, $location, $timeout,$state, pathService, paymentsService, transactionService) {
 
- 	$scope.params = {
-        pageNumber: 1,
-        pageSize: 10
+    $scope.params = {
+        statusPaymentCriteria: "waiting",
+        paymentSummaryScopeType: "all"
     };
+
+    $scope.paymentsPromise = paymentsSummaryService.search($scope.params).then(function(paymentSummary) {
+        $scope.paymentSummary = paymentSummary;
+    });
+
+    $scope.submit = function(ctx, params, rel) {
+        return paymentsService.relationSearch(ctx, params, rel).then(function(paymentSummary) {
+            return paymentSummary;
+        });
+    };
+
+
     $scope.widgetContent = pathService.generateTemplatePath("raiffeisen-payments") + '/layouts/widget/widget_payments_content.html';
+    $scope.options = {
+        detailsShown: false
+    };
+
 
 });
