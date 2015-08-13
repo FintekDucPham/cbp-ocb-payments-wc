f7382960a3021118d4ae1eca8ebb50fe8f286eb0
Mariusz.Kaczmarczyk <mariusz.kaczmarczyk@asseco.pl>
2015-08-13 21:58:51 +0200

    Dodanie listy platnosci zaplanowanych.

--- src/main/layouts/widget/widget_payments.js
+++ src/main/layouts/widget/widget_payments.js
@@ -1,19 +1,13 @@
-angular.module('raiffeisen-payments').controller('PaymentsWidgetController', function($scope, paymentsSummaryService, $rootScope, $location, $timeout,$state, pathService, paymentsService, transactionService) {
+angular.module('raiffeisen-payments').controller('PaymentsWidgetController', function($scope, paymentsService, $rootScope, $location, $timeout,$state, pathService, paymentsService, transactionService) {
 
     $scope.params = {
         statusPaymentCriteria: "waiting",
         paymentSummaryScopeType: "all"
     };
 
-    $scope.paymentsPromise = paymentsSummaryService.search($scope.params).then(function(paymentSummary) {
+    $scope.paymentsPromise = paymentsService.search($scope.params).then(function(paymentSummary) {
         $scope.paymentSummary = paymentSummary;
     });
-
-    $scope.submit = function(ctx, params, rel) {
-        return paymentsService.relationSearch(ctx, params, rel).then(function(paymentSummary) {
-            return paymentSummary;
-        });
-    };
 
 
     $scope.widgetContent = pathService.generateTemplatePath("raiffeisen-payments") + '/layouts/widget/widget_payments_content.html';
