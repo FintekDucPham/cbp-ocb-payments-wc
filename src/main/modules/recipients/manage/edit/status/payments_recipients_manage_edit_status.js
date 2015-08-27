d72eaa2a67d76d44d2814dad6d02f805867e093f
dawid <d.kurekPL@gmail.com>
2015-08-27 11:31:28 +0200

    dodano sprawdzania statusu na szynie

--- src/main/modules/recipients/manage/edit/status/payments_recipients_manage_edit_status.js
+++ src/main/modules/recipients/manage/edit/status/payments_recipients_manage_edit_status.js
@@ -1,4 +1,21 @@
 angular.module('raiffeisen-payments')
-    .controller('RecipientsManageEditStatusController', function ($scope) {
+    .controller('RecipientsManageEditStatusController', function ($scope, bdStepStateEvents, translate) {
+        $scope.$on(bdStepStateEvents.ON_STEP_ENTERED, function (event, actions) {
+            if($scope.recipient.result.type == 'success'){
+                prepareResult('raiff.payments.recipients.manage.edit.status.ok');
+            }else {
+                prepareResult('raiff.payments.recipients.manage.edit.status.fail');
+            }
+
+        });
+
+
+        function prepareResult(msg) {
+            $scope.recipient.result.text = parseMessage(msg);
+        }
+
+        function parseMessage(msg) {
+            return translate.property(msg);
+        }
 
     });
