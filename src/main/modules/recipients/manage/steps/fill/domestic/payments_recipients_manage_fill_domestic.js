d32011b792a9e8186e9221a6d0ed12d3534d1d19
Grzegorz Gurgul <grzegorz.gurgul@asseco.pl>
2015-08-31 12:07:51 +0200

    ZD01244698

--- src/main/modules/recipients/manage/steps/fill/domestic/payments_recipients_manage_fill_domestic.js
+++ src/main/modules/recipients/manage/steps/fill/domestic/payments_recipients_manage_fill_domestic.js
@@ -1,13 +1,46 @@
 angular.module('raiffeisen-payments')
-    .controller('RecipientsManageFillDomesticController', function ($scope, lodash, bdStepStateEvents, formService, rbAccountSelectParams, recipientGeneralService) {
+    .controller('RecipientsManageFillDomesticController', function ($scope, lodash, bdStepStateEvents, formService, rbAccountSelectParams, taxOffices) {
 
         $scope.onSenderAccountSelect = function () {
             $scope.recipientForm.recipientAccountNo.$validate();
         };
 
+
+        $scope.recipient.meta.recipientForbiddenAccounts = lodash.union($scope.recipient.meta.recipientForbiddenAccounts, lodash.map([
+            "83101010230000261395100000",
+            "78101010230000261395200000",
+            "73101010230000261395300000"
+        ], function (val) {
+            return {
+                code: 'notZus',
+                value: val
+            };
+        }));
+
         $scope.$on('clearForm', function () {
             if($scope.recipientForm) {
                 formService.clearForm($scope.recipientForm);
+            }
+        });
+
+        $scope.$on(bdStepStateEvents.BEFORE_FORWARD_MOVE, function (event, control) {
+            if($scope.recipientForm.recipientAccountNo.$valid) {
+                control.holdOn();
+                taxOffices.search({
+                    accountNo: $scope.recipient.formData.recipientAccountNo
+                }).then(function (result) {
+                    if (result.length > 0) {
+                        lodash.forEach(result, function(value) {
+                            $scope.recipient.meta.recipientForbiddenAccounts = lodash.union($scope.recipient.meta.recipientForbiddenAccounts, [
+                                {
+                                    code: 'notUs',
+                                    value: value.accountNo
+                                }
+                            ]);
+                        });
+                        $scope.recipientForm.recipientAccountNo.$validate();
+                    }
+                }).finally(control.done);
             }
         });
 
@@ -20,13 +53,32 @@ angular.module('raiffeisen-payments')
                     actions.proceed();
                 }
             }
-
         });
 
         $scope.recipientAccountValidators = {
             sameAccount: function (accountNo) {
                 var senderAccount = $scope.recipient.items.senderAccount;
                 return !accountNo || !senderAccount || senderAccount.accountNo !== accountNo.replace(/ /g, '');
+            },
+            notUs: function (accountNo) {
+                if (accountNo) {
+                    return !lodash.some($scope.recipient.meta.recipientForbiddenAccounts, {
+                        code: 'notUs',
+                        value: accountNo.replace(" ", '')
+                    });
+                } else {
+                    return false;
+                }
+            },
+            notZus: function (accountNo) {
+                if (accountNo) {
+                    return !lodash.some($scope.recipient.meta.recipientForbiddenAccounts, {
+                        code: 'notZus',
+                        value: accountNo.replace(" ", '')
+                    });
+                } else {
+                    return false;
+                }
             }
         };
 
@@ -43,4 +95,5 @@ angular.module('raiffeisen-payments')
                 });
             }
         });
+
     });
