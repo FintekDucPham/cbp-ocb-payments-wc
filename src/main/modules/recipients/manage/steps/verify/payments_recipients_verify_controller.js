eacaf02898df806c8f669c0a148356833ab3703f
Grzegorz Gurgul <grzegorz.gurgul@asseco.pl>
2015-09-09 10:52:14 +0200

    poprawka

--- src/main/modules/recipients/manage/steps/verify/payments_recipients_verify_controller.js
+++ src/main/modules/recipients/manage/steps/verify/payments_recipients_verify_controller.js
@@ -1,6 +1,6 @@
 angular.module('raiffeisen-payments')
     .controller('RecipientsManageVerifyController', function ($scope, pathService, recipientGeneralService, authorizationService,
-                                                              formService, bdStepStateEvents) {
+                                                              formService, bdStepStateEvents, translate, dateFilter) {
 
         $scope.recipientAuthUrl = pathService.generateTemplatePath("raiffeisen-payments") + "/modules/recipients/manage/verify/payments_recipients_auth.html";
 
