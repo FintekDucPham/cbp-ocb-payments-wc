e7b13f70a9d3e7b57139274e42a6a3f52d80bbf4
Grzegorz Gurgul <grzegorz.gurgul@asseco.pl>
2015-09-28 12:30:01 +0200

    odbiorcy tax na formatce

---
+++ src/main/components/rbTaxRecipientSelect/rbTaxRecipientSelect.js
@@ -0,0 +1,75 @@
+angular.module('raiffeisen-payments')
+    .directive('rbTaxRecipientSelect', function (pathService, recipientsService, lodash) {
+        return {
+            restrict: 'E',
+            templateUrl: pathService.generateTemplatePath("raiffeisen-payments") + "/components/rbTaxRecipientSelect/rbTaxRecipientSelect.html",
+            scope: {
+                recipientId: '=rbTaxRecipientId',
+                recipient: '=rbTaxRecipient',
+                recipientList: '=?rbTaxRecipientList',
+                placeholderText: '@rbPlaceholderText',
+                clearText: '@rbRecipientClearText',
+                onSelectRecipient: '&rbOnSelectRecipient',
+                onClearRecipient: '&rbOnClearRecipient'
+            },
+            controller: function ($scope) {
+
+                $scope.selection = {
+                    isSelected: false
+                };
+
+                $scope.selectRecipient = function($item) {
+                    if($item) {
+                        $scope.selection.isSelected = true;
+                        var oldRecipient = $scope.recipient;
+                        $scope.recipient = $item;
+                        $scope.onSelectRecipient({
+                            $recipient: $item,
+                            $oldRecipient: oldRecipient
+                        });
+                    } else {
+                        $scope.recipient = null;
+                        $scope.onClearRecipient();
+                    }
+                };
+
+                $scope.$watch('recipient', function(recipient) {
+                    $scope.selection.recipient = recipient;
+                    $scope.selection.isSelected = !!recipient;
+                });
+
+                recipientsService.search({
+                    filerTemplateType: 'TAX'
+                }).then(function(data) {
+                    $scope.recipientList = lodash.union([ nullOption ], lodash.map(data.content, function(data) {
+                        var template = data.paymentTemplates[0];
+                        var paymentDetails = template.paymentDetails;
+                        return {
+                            templateId: data.recipientId,
+                            customerName: data.recipientName.join(" "),
+                            recipientId: data.recipientId,
+                            recipientName: data.recipientAddress,
+                            nrb: template.beneficiaryAccountNo,
+                            debitNrb: template.remitterAccountNo,
+                            secondaryIdType: paymentDetails.idtype,
+                            secondaryId: paymentDetails.idnumber,
+                            periodType: paymentDetails.periodType,
+                            formCode: paymentDetails.formCode
+                        };
+                    }));
+                });
+
+                $scope.clearSelection = function () {
+                    $scope.selection.recipient = null;
+                    $scope.recipient = null;
+                    $scope.onClearRecipient();
+                    $scope.selection.isSelected = false;
+                };
+
+                var nullOption = $scope.nullOption = {
+                    customerName: 'Odbiorca spoza listy'
+                };
+
+            }
+        };
+    });
