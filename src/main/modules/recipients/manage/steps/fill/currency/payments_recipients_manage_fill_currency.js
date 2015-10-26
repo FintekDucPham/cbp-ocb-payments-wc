ec5d015785cebd85c1edf7e06f1ed9726a588863
Wojciech Brygola <wojciech.brygola@asseco.pl>
2015-10-26 16:20:02 +0100

    currency recipients draft

---
+++ src/main/modules/recipients/manage/steps/fill/currency/payments_recipients_manage_fill_currency.js
@@ -0,0 +1,79 @@
+angular.module('raiffeisen-payments')
+    .controller('RecipientsManageFillDomesticController', function ($scope, notInsuranceAccountGuard, notTaxAccountGuard, lodash, bdStepStateEvents, formService, rbAccountSelectParams, translate, customerService, accountsService) {
+
+        $scope.recipient.meta.forbiddenAccounts = [];
+
+        var recipientValidators = {
+            tax: notTaxAccountGuard($scope.recipient.meta),
+            insurance:  notInsuranceAccountGuard($scope.recipient.meta)
+        };
+        customerService.getCustomerDetails().then(function(customerDetails){
+            $scope.customerDetails = customerDetails.customerDetails;
+        });
+        $scope.accountListPromise = accountsService.search().then(function(accountList){
+            $scope.accountsList = accountList.content;
+        });
+        $scope.getAccountByNrb = function(accountNumber){
+            return lodash.find($scope.accountsList, {
+                accountNo: accountNumber
+            });
+        };
+        $scope.onSenderAccountSelect = function () {
+            $scope.recipientForm.recipientAccountNo.$validate();
+        };
+
+        $scope.$on('clearForm', function () {
+            if($scope.recipientForm) {
+                formService.clearForm($scope.recipientForm);
+            }
+        });
+
+        $scope.$on(bdStepStateEvents.BEFORE_FORWARD_MOVE, function (event, control) {
+            if($scope.recipientForm.recipientAccountNo.$valid) {
+                control.holdOn();
+                var recipientAccountNo = $scope.recipient.formData.recipientAccountNo;
+                recipientValidators.insurance.validate(recipientAccountNo, function() {
+                    recipientValidators.tax.validate(recipientAccountNo, function() {
+                        $scope.recipientForm.recipientAccountNo.$validate();
+                        control.done();
+                    });
+                });
+            }
+        });
+
+        $scope.recipientAccountValidators = {
+            sameAccount: function (accountNo) {
+                var senderAccount = $scope.recipient.items.senderAccount;
+                return !accountNo || !senderAccount || senderAccount.accountNo !== accountNo.replace(/ /g, '');
+            },
+            notUs: recipientValidators.tax.getValidator(),
+            notZus: recipientValidators.insurance.getValidator()
+        };
+
+        $scope.recipientSelectParams = new rbAccountSelectParams({
+            messageWhenNoAvailable: translate.property('raiff.payments.recipients.new.domestic.fill.account_related.none_available'),
+            useFirstByDefault: true,
+            alwaysSelected: false,
+            accountFilter: function (accounts) {
+               return accounts;
+            },
+            decorateRequest: function(params){
+                return angular.extend(params, {
+                    currency: "PLN",
+                    productList: "BENEFICIARY_CREATE_FROM_LIST"
+                });
+            }
+        });
+
+        $scope.setRequestConverter(function (formData) {
+            var copiedFormData = JSON.parse(JSON.stringify(formData));
+            return {
+                shortName: copiedFormData.customName,
+                debitAccount: copiedFormData.remitterAccountId,
+                creditAccount: copiedFormData.recipientAccountNo,
+                beneficiary: copiedFormData.recipientData,
+                remarks: copiedFormData.description
+            };
+        });
+
+    });
