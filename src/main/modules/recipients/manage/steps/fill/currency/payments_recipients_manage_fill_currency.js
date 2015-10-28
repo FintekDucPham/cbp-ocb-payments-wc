6b771f018b85822dcc58cef8ed944bc634161162
Wojciech Brygola <wojciech.brygola@asseco.pl>
2015-10-28 10:51:57 +0100

    commit as backup - still draft version

--- src/main/modules/recipients/manage/steps/fill/currency/payments_recipients_manage_fill_currency.js
+++ src/main/modules/recipients/manage/steps/fill/currency/payments_recipients_manage_fill_currency.js
@@ -1,23 +1,59 @@
 angular.module('raiffeisen-payments')
-    .controller('RecipientsManageFillDomesticController', function ($scope, notInsuranceAccountGuard, notTaxAccountGuard, lodash, bdStepStateEvents, formService, rbAccountSelectParams, translate, customerService, accountsService) {
+    .constant("RECIPIENT_IDENTITY_TYPES", {
+        "BIC_OR_SWIFT": "BIC_OR_SWIFT",
+        "NAME_AND_COUNTRY": "NAME_AND_COUNTRY"
+    })
+    .controller('PaymentsRecipientsManageFillCurrencyController', function ($q, $scope, notInsuranceAccountGuard, notTaxAccountGuard, lodash, bdStepStateEvents, formService, rbAccountSelectParams, translate, customerService, accountsService, RECIPIENT_IDENTITY_TYPES) {
+
+        $scope.recipientIdentityTypes = RECIPIENT_IDENTITY_TYPES;
 
         $scope.recipient.meta.forbiddenAccounts = [];
+
+        /** BEGIN MOCK DATA **/
+        $scope.ibanValidationRulesPerCountry = [
+            { "COUNTRY_CODE": "PL", "IBAN_LENGTH": 24},
+            { "COUNTRY_CODE": "EN", "IBAN_LENGTH": 24},
+            { "COUNTRY_CODE": "DE", "IBAN_LENGTH": 24},
+            { "COUNTRY_CODE": "SR", "IBAN_LENGTH": 24}
+        ];
+
+        $scope.countries = [
+            { "CODE": "PL", "DESCRIPTION": "ddd", "SHORT_DESCRIPTION": "", "GEOGRAPHICAL_BLOCK": "", "LANGUAGE": "", "CURRENCY_CODE": ""},
+            { "CODE": "EN", "DESCRIPTION": "ddd", "SHORT_DESCRIPTION": "", "GEOGRAPHICAL_BLOCK": "", "LANGUAGE": "", "CURRENCY_CODE": ""},
+            { "CODE": "DE", "DESCRIPTION": "ddd", "SHORT_DESCRIPTION": "", "GEOGRAPHICAL_BLOCK": "", "LANGUAGE": "", "CURRENCY_CODE": ""},
+            { "CODE": "SR", "DESCRIPTION": "ddd", "SHORT_DESCRIPTION": "", "GEOGRAPHICAL_BLOCK": "", "LANGUAGE": "", "CURRENCY_CODE": ""}
+        ];
+
+        $scope.swiftCodes = [
+            { 'CODE': "134234", "NAME": "BANK #1" },
+            { 'CODE': "234234", "NAME": "BANK #2" },
+            { 'CODE': "334234", "NAME": "BANK #3" },
+            { 'CODE': "4344234", "NAME": "BANK #4" },
+            { 'CODE': "534234", "NAME": "BANK #5" },
+            { 'CODE': "634234", "NAME": "BANK #6" }
+        ];
+
+        /** END MOCK DATA **/
 
         var recipientValidators = {
             tax: notTaxAccountGuard($scope.recipient.meta),
             insurance:  notInsuranceAccountGuard($scope.recipient.meta)
         };
+
         customerService.getCustomerDetails().then(function(customerDetails){
             $scope.customerDetails = customerDetails.customerDetails;
         });
+
         $scope.accountListPromise = accountsService.search().then(function(accountList){
             $scope.accountsList = accountList.content;
         });
+
         $scope.getAccountByNrb = function(accountNumber){
             return lodash.find($scope.accountsList, {
                 accountNo: accountNumber
             });
         };
+
         $scope.onSenderAccountSelect = function () {
             $scope.recipientForm.recipientAccountNo.$validate();
         };
