65d2344608c8fd4fd45bb4237a785ee2a1950230
marek.koronski <marek.koronski@asseco.pl>
2015-11-19 13:22:53 +0100

    edit init

---
+++ src/main/modules/future/manage/steps/fill/domestic/payments_future_manage_fill_domestic.js
@@ -0,0 +1,6 @@
+angular.module('raiffeisen-payments')
+    .controller('FutureManageFillDomesticController', function ($scope, notInsuranceAccountGuard, notTaxAccountGuard, lodash, bdStepStateEvents, formService, rbAccountSelectParams, translate, customerService, accountsService) {
+
+
+
+    });
