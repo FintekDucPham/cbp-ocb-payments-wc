7802bac7bd295f7c33165b69b16a9814dce511d4
Wojciech Brygola <wojciech.brygola@asseco.pl>
2015-11-23 15:39:12 +0100

    cleanup

--- src/main/modules/standing/payments_future.js
+++
@@ -1,7 +0,0 @@
-angular.module('raiffeisen-payments')
-    .config(function (pathServiceProvider, stateServiceProvider) {
-        stateServiceProvider.state('payments.future', {
-            url: "/future",
-            templateUrl: pathServiceProvider.generateTemplatePath("raiffeisen-payments") + "/modules/future/payments_future.html"
-        });
-    });
