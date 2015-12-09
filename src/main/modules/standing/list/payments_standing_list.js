2ed448d1e9332e6da6725620d859ae480d430454
Wojciech Brygola <wojciech.brygola@asseco.pl>
2015-12-09 12:58:34 +0100

    popr 1/4 -> komunikaty walidacji daty pierwszej realizacji przy modyfikacji zlecenia

--- src/main/modules/standing/list/payments_standing_list.js
+++ src/main/modules/standing/list/payments_standing_list.js
@@ -102,14 +102,12 @@ angular.module('raiffeisen-payments')
 
                     standingTransferService.search(params).then(function (response) {
 
+
                         response.content = response.content.map(function(elem) {
-                            elem.frequency_nextDate = elem.standingOrder.frequency.nextDate;
-                            elem.shortName = elem.standingOrder.shortName;
-                            elem.amount = elem.standingOrder.amount;
-                            elem.currency = elem.standingOrder.currency;
-                            elem.beneficiary = elem.standingOrder.beneficiary;
-                            elem.creditAccount = elem.standingOrder.creditAccount;
-                            return elem;
+                            elem.standingOrder.bankName = elem.bankName;
+                            elem.standingOrder.frequency_nextDate = elem.standingOrder.frequency.nextDate;
+
+                            return elem.standingOrder;
                         });
 
                         defer.resolve(response.content);
