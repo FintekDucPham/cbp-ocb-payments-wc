8bd518f7ec9cb120fb7a661e457cde82b68ecbc4
Wojciech Brygola <wojciech.brygola@asseco.pl>
2015-12-11 14:06:54 +0100

    OZK219006 - przeniesienie getInsuranceAccountName wyżej aby był dostępny dla wszystkich formatek

--- src/main/modules/future/payments_future.js
+++ src/main/modules/future/payments_future.js
@@ -2,6 +2,24 @@ angular.module('raiffeisen-payments')
     .config(function (pathServiceProvider, stateServiceProvider) {
         stateServiceProvider.state('payments.future', {
             url: "/future",
-            templateUrl: pathServiceProvider.generateTemplatePath("raiffeisen-payments") + "/modules/future/payments_future.html"
+            templateUrl: pathServiceProvider.generateTemplatePath("raiffeisen-payments") + "/modules/future/payments_future.html",
+            controller: "PaymentsFutureController",
+            abstract: true,
+            resolve: {
+                insuranceAccountList : ['insuranceAccounts', function(insuranceAccounts){
+                    return insuranceAccounts.search().then(function(insuranceAccounts) {
+                        return insuranceAccounts.content;
+                    });
+                }]
+            }
         });
+    })
+    .controller("PaymentsFutureController", function($scope, translate, insuranceAccountList) {
+        $scope.insuranceAccounts = insuranceAccountList;
+        $scope.getInsuranceAccountName = function(accountNo){
+            var foundElement = _.find($scope.insuranceAccounts, {
+                accountNo: accountNo
+            });
+            return translate.property("raiff.payments.insurances.type."+foundElement.insuranceCode);
+        };
     });
