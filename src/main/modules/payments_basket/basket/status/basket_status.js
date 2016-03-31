10ce273c832fc53210583d4c05fe583a872d4f8c
dawid <d.kurekPL@gmail.com>
2016-03-31 17:39:09 +0200

    Badanie czy przelewy nie mają przekroczonej daty realizacji JIRA WIEL-61

--- src/main/modules/payments_basket/basket/status/basket_status.js
+++ src/main/modules/payments_basket/basket/status/basket_status.js
@@ -7,15 +7,38 @@ angular.module('raiffeisen-payments')
         });
     })
     .controller('PaymentsBasketStatusController', function ($scope, $state, $timeout, $q, translate, $filter) {
+        $scope.validationMsg = {
+            message : "",
+            show: false
+        };
+
         var transactionSum = $scope.basket.item.result.notAcceptedTransactions + $scope.basket.item.result.readyTransactions;
-        if($scope.basket.item.result.message == "AMOUNT_EXCEEDED_FUNDS"){
-            $scope.validationMsg = translate.property('raiff.payments.basket.status.AMOUNT_EXCEEDED_FUNDS', [$scope.basket.item.result.readyTransactions, transactionSum]);
+        if($scope.basket.item.result.messages.indexOf("AMOUNT_EXCEEDED_FUNDS") > -1){
+            $scope.validationMsg.message = translate.property('raiff.payments.basket.status.AMOUNT_EXCEEDED_FUNDS', [$scope.basket.item.result.readyTransactions, transactionSum]);
+            $scope.validationMsg.show = true;
         }
-        if($scope.basket.item.result.message == "DAILY_LIMIT_EXCEEDED"){
-            $scope.validationMsg = translate.property('raiff.payments.basket.status.DAILY_LIMIT_EXCEEDED', [$scope.basket.item.result.readyTransactions, transactionSum]);
+        if($scope.basket.item.result.messages.indexOf("DAILY_LIMIT_EXCEEDED") > -1){
+            $scope.validationMsg.message = translate.property('raiff.payments.basket.status.DAILY_LIMIT_EXCEEDED', [$scope.basket.item.result.readyTransactions, transactionSum]);
+            $scope.validationMsg.show = true;
         }
         if($scope.basket.item.result.notAcceptedTransactions > 0){
-            $scope.notAcceptedTransactionMsg = translate.property('raiff.payments.basket.status.NOT_ACCEPTED_TRANSACTIONS', [$scope.basket.item.result.notAcceptedTransactions, transactionSum]);
+            $scope.notAcceptedTransactionMsg = {
+                message: translate.property('raiff.payments.basket.status.NOT_ACCEPTED_TRANSACTIONS', [$scope.basket.item.result.notAcceptedTransactions, transactionSum]),
+                show:true
+            };
         }
+        if($scope.basket.item.result.messages.indexOf("REALIZATION_DATE_EXCEEDED") > -1){
+            $scope.realizationDateExceeded = {
+                message: translate.property('raiff.payments.basket.status.REALIZATION_DATE_EXCEEDED'),
+                show:true
+            };
+        }
+        if($scope.basket.item.result.transactionsSubmited){
+            $scope.transactionsSubmited = {
+                message: translate.property('raiff.payments.basket.status.TRANSACTIONS_SUBMITED', [$scope.basket.item.result.readyTransactions, transactionSum]),
+                show:true
+            };
+        }
+
 
     });
