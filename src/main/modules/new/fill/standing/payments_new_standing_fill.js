4aba8f0f262065eac8f07bddc7cf8aca9ee3b691
Bartosz Mrowka <Bartosz.Mrowka@aps00062821-mb.poland.asseco.corp>
2016-07-13 12:20:42 +0200

    ZD01354550 - fix

--- src/main/modules/new/fill/standing/payments_new_standing_fill.js
+++ src/main/modules/new/fill/standing/payments_new_standing_fill.js
@@ -240,4 +240,10 @@ angular.module('raiffeisen-payments')
                 //return senderAccount && recipient.srcAccountNo === senderAccount.accountNo.replace(/ /g, '');
             }
         };
+
+        $scope.$watch('payment.formData.firstRealizationDate', function(n,o){
+            if(n!==o){
+                $scope.validationErrors.firstRealizationDate = undefined;
+            }
+        });
     });
