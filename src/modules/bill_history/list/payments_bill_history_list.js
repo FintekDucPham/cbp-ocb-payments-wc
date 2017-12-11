6eb9f82772c28b2c39b02f598f8bd5b31d814c61
ttlong <long.tran@fintek.vn>
2017-12-11 22:43:08 +0700

    fix paging

--- src/modules/bill_history/list/payments_bill_history_list.js
+++ src/modules/bill_history/list/payments_bill_history_list.js
@@ -9,24 +9,22 @@ angular.module('ocb-payments')
             }
         });
     })
-    .controller('PaymentsBillHistoryListController', function ($scope,bdTableConfig, translate, $filter, exportService, fileDownloadService, transferBillService, creditsService) {
+    .controller('PaymentsBillHistoryListController', function ($scope, bdTableConfig, translate, $filter, exportService, fileDownloadService, transferBillService, creditsService) {
         //Set data bill history table
 
         //table config
-        $scope.tableConfig = new bdTableConfig({
-
-        });
+        $scope.tableConfig = new bdTableConfig({});
 
         //table def
         $scope.table = {
             tableConfig: new bdTableConfig({
                 placeholderText: $filter('translate')('ocb.payments.PaymentsBillHistory'),
-                downloadFile: function(item) {
+                downloadFile: function (item) {
                     var downloadLink = "/api/account/downloads/account_electronic_invoice_download.json",
                         url = exportService.prepareHref(downloadLink);
                     fileDownloadService.startFileDownload(url);
                 },
-                exportpdf: function() {
+                exportpdf: function () {
                     html2canvas(document.getElementById('transactionDetail'), {
                         onrendered: function (canvas) {
                             var data = canvas.toDataURL();
@@ -41,36 +39,38 @@ angular.module('ocb-payments')
                     });
                 },
                 hideAddress: function (billType) {
-                    if (billType == "EXTENDED_DETAIL" || billType == "NO_DETAIL" ){
+                    if (billType == "EXTENDED_DETAIL" || billType == "NO_DETAIL") {
                         return true;
                     }
                     return false;
                 },
                 hideQty: function (billType) {
-                    if (billType == "MASTER_DETAIL" || billType == "NO_DETAIL"){
+                    if (billType == "MASTER_DETAIL" || billType == "NO_DETAIL") {
                         return true;
                     }
                     return false;
                 },
                 hideFullName: function (billType) {
-                    if (billType == "NO_DETAIL"){
+                    if (billType == "NO_DETAIL") {
                         return true;
                     }
                     return false;
                 }
             }),
             tableData: {
-                // getData : $scope.billHistoryData
                 getData: function (defer, $params) {
                     /*Test calendar Start*/
-                    var fromDay = $scope.rbModelFrom;
-                    $scope.billHistoryData = transferBillService.getBillHistory({fromDate : '2017-01-01', toDate: '2017-12-30'}).then(function(data) {
-                       // defer.resolve(data.content);
+                    //var fromDay = rbModelFrom;
+                    $scope.billHistoryData = transferBillService.getBillHistory({
+                        fromDate: '2017-01-01',
+                        toDate: '2017-12-30'
+                    }).then(function (data) {
+                        // defer.resolve(data.content);
 
                         /*TEST Start*/
                         var totalItems = data.content.length;
                         // init page size
-                        var pageSize = pageSize || 5;
+                        var pageSize = pageSize || 10;
                         var currentPage = $params.currentPage;
                         var totalPages = Math.ceil(totalItems / pageSize);
                         var startPage, endPage;
@@ -98,61 +98,15 @@ angular.module('ocb-payments')
 
                         $scope.targetList = angular.copy(data.content);
                         $scope.targetList.content = [];
-                        for ( i = startIndex; i <= endIndex ; i++ ){
+                        for (i = startIndex; i <= endIndex; i++) {
                             var selectedItem = data.content[i];
                             $scope.targetList.content.push(selectedItem);
 
                         }
                         defer.resolve($scope.targetList.content);
-                        $params.pageCount = 2;
-
-
-
-
-
+                        $params.pageCount = totalPages;
                     });
-                   //  var totalItems = $scope.billHistoryData.length;
-                   //  // init page size
-                   //  var pageSize = pageSize || 5;
-                   //  var currentPage = $params.currentPage;
-                   //  var totalPages = Math.ceil(totalItems / pageSize);
-                   //  var startPage, endPage;
-                   //  if (totalPages <= 10) {
-                   //      // less than 10 total pages so show all
-                   //      startPage = 1;
-                   //      endPage = totalPages;
-                   //  } else {
-                   //      // more than 10 total pages so calculate start and end pages
-                   //      if (currentPage <= 6) {
-                   //          startPage = 1;
-                   //          endPage = 10;
-                   //      } else if (currentPage + 4 >= totalPages) {
-                   //          startPage = totalPages - 9;
-                   //          endPage = totalPages;
-                   //      } else {
-                   //          startPage = currentPage - 5;
-                   //          endPage = currentPage + 4;
-                   //      }
-                   //  }
-                   //
-                   //  // calculate start and end item indexes
-                   //  var startIndex = (currentPage - 1) * pageSize;
-                   //  var endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
-                   //
-                   //  $scope.targetList = angular.copy($scope.billHistoryData);
-                   //  // $scope.targetList.content = [];
-                   //  for ( i = startIndex; i <= endIndex ; i++ ){
-                   //      var selectedItem = $scope.targetList[i];
-                   //      // TODO
-                   //      // $scope.targetList.push(selectedItem);
-                   //  }
-                   //  defer.resolve($scope.billHistoryData);
-                   // // $scope.table.anyData = billsList.content[0].billItem.length > 0;
-                   //  $params.pageCount = $scope.billHistoryData.totalPages;
-
-
                 }
-
             },
             tableControl: undefined
         };
