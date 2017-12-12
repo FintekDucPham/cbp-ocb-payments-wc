f6705123ac1dc3122df74ff34167b44d4d4dc8f2
BSV <info@bsv-consulting.sk>
2017-12-12 18:06:21 +0100

    dev

--- src/components/rbBankSelect/rbBankSelect.js
+++ src/components/rbBankSelect/rbBankSelect.js
@@ -1,44 +1,90 @@
-ebankingSharedModule.directive('rbBankSelect', function(pathService, $timeout) {
+ebankingSharedModule.directive('rbBankSelect', function(pathService, domesticBanksService) {
     'use strict';
 
     return {
         restrict: 'E',
         templateUrl: pathService.generateTemplatePath('ocb-payments') + '/components/rbBankSelect/rbBankSelect.html',
-        require: '^rbRecipientLocation',
+        require: '?^rbRecipientLocation',
         scope: {
             bankCode: '=rbBankCode',
             bank: '=rbBank',
-            //onSelect: '&rbOnBankSelect',
+            placeholder: '@?rbPlaceholder'
         },
 
         link: function postLink($scope, iElement, iAttrs, locationCtrl) {
-            $scope.locationCtrl = locationCtrl;
+            $scope.loading = true;
+            $scope.banksList = null;
             $scope.selection = {};
 
-            locationCtrl.loadingPromise.then(function () {
-                $scope.$watch('bank', function (bankToSelect) {
-                    $scope.bankCode = bankToSelect.bankCode;
-                    $scope.selection.bank = bankToSelect;
-                    locationCtrl.bankList.some(function (bank) {
-                        if (bank.bankCode === bankToSelect.bankCode) {
-                            $scope.bank = bank;
-                            $scope.selection.bank = bank;
-                            return true;
-                        }
+            $scope.loadingPromise = locationCtrl
+                ? locationCtrl.loadingPromise.then(function () {
+                    $scope.banksList = locationCtrl.banksList;
+                })
+                : domesticBanksService.search({}).then(function(data) {
+                    $scope.banksList =  data.content.banksList.map(function (bank) {
+                        return {
+                            bankCode: bank.unitNo,
+                            bankName: bank.nameShort
+                        };
                     });
                 });
+
+            $scope.loadingPromise.then(function () {
+                $scope.$watch('bankCode', function (bankCode) {
+                    if (!bankCode) {
+                        clearSelection();
+                    } else if (!selectBank(bankCode)) {
+                        setBank({
+                            bankCode: bankCode,
+                            bankName: bankCode
+                        });
+                    }
+                });
+                $scope.$watch('bank', function (bank) {
+                    if (!bank) {
+                        clearSelection();
+                    } else if (!selectBank(bank.bankCode)) {
+                        setBank(bank);
+                    }
+                });
+                $scope.loading = false;
             });
 
+
             $scope.selectBank = function (bank) {
+                if (!bank) {
+                    clearSelection();
+                } else {
+                    setBank(bank);
+                }
+            };
+
+            function clearSelection() {
+                $scope.selection.bank = null;
+                $scope.bankCode = '';
+                $scope.bank = null;
+                if (locationCtrl) {
+                    locationCtrl.setBank(null);
+                }
+            }
+
+            function setBank(bank) {
+                $scope.selection.bank = bank;
                 $scope.bankCode = bank.bankCode;
                 $scope.bank = bank;
-                $scope.selection.bank = bank;
-                $timeout(function() {
-                    // $scope.onSelect({
-                    //     $bank: bank
-                    // });
+                if (locationCtrl) {
+                    locationCtrl.setBank(bank);
+                }
+            }
+
+            function selectBank(bankCode) {
+                return $scope.banksList.some(function (bank) {
+                    if (bank.bankCode === bankCode) {
+                        setBank(bank);
+                        return true;
+                    }
                 });
-            };
+            }
         }
     };
 });
