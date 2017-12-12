f6814f6e6269a94965cb1114f6b13c7d1170d47d
BSV <info@bsv-consulting.sk>
2017-12-12 18:06:21 +0100

    dev

--- src/components/rbRecipientLocation/rbRecipientLocation.js
+++ src/components/rbRecipientLocation/rbRecipientLocation.js
@@ -4,19 +4,61 @@ ebankingSharedModule.directive('rbRecipientLocation', function(domesticBanksServ
     return {
         restrict: 'EA',
 
-        controller: function () {
-            var ctrl = this;
-            ctrl.loadingPromise = domesticBanksService.search({
-            }).then(function(data) {
-                ctrl.bankList = data.content.map(function (bank) {
+        controller: function ($scope) {
+            var ctrl = this, banksList = null, branchBankCode = '', branchProvince = '';
+            ctrl.banksList = null;
+            ctrl.branchesList = null;
+            ctrl.loading = true;
+
+            ctrl.loadingPromise = domesticBanksService.search({}).then(function(data) {
+                banksList = data.content;
+                ctrl.banksList = banksList.map(function (bank) {
                     return {
                         bankCode: bank.unitNo,
                         bankName: bank.nameShort
                     }
                 });
+                updateBranchList();
             }).finally(function () {
-                ctrl.loadingPromise = null;
+                ctrl.loading = false;
             });
+
+            ctrl.setBank = function (bank) {
+                var bankCode = bank ? bank.bankCode : '';
+                if (branchBankCode !== bankCode) {
+                    branchBankCode = bankCode;
+                    if (banksList) {
+                        updateBranchList();
+                    }
+                }
+            };
+
+            ctrl.setProvince = function (province) {
+                if (branchProvince !== province) {
+                    branchProvince = province;
+                    if (banksList) {
+                        updateBranchList();
+                    }
+                }
+            };
+
+            function updateBranchList() {
+                var branchesList = [];
+                banksList.forEach(function (bank) {
+                    if (branchBankCode === bank.unitNo) {
+                        bank.branches.forEach(function (branch) {
+                           if (branchProvince === branch.province) {
+                               branchesList.push({
+                                   branchCode: branch.branchCode,
+                                   branchName: branch.branchName
+                               });
+                           }
+                        });
+                    }
+                });
+                ctrl.branchesList = branchesList;
+                $scope.$broadcast('BranchesListUpdated');
+            }
         }
     };
 });
