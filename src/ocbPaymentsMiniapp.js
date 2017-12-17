7c1eb8d09008cd2695cf4a623111f87ac883c772
ext_susol <ivan.susol@bsv-consulting.sk>
2017-12-17 20:36:48 +0100

    Accumulative saving

--- src/ocbPaymentsMiniapp.js
+++ src/ocbPaymentsMiniapp.js
@@ -58,6 +58,21 @@ angular.module('ocb-payments', [
                     icon: "ocb-icons ocb_przelew",
                     action: "payments.new_internal.fill",
                     priority: 1
+                },{
+                    id: "payments.new.saving",
+                    label: 'ocb.payments.future.saving.label',
+                    icon: "ocb-icons ocb_odbiorcy",
+                    action: function(item, scope, state){
+                        state.reload('payments.new.fill').then(function(){
+                            state.transitionTo("payments.new.fill",{ paymentType: 'saving', referenceId: undefined }, {reload: true}).finally(function() {
+                                state.go('payments.new.fill', {
+                                    paymentType: 'saving',
+                                    referenceId: undefined
+                                });
+                            });
+                        });
+                    },
+                    priority: 2
                 },
                 {
                     id: "payments.recipients.list",
