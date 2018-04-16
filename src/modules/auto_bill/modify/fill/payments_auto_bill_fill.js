62b0eeac2ebfe40b10bc1df096e22582a0cc0de2
jludik <juraj.ludik@sigp.eu>
2018-04-16 10:01:03 +0200

    OCBVNMCP-3693, OCBVNMCP-3700

--- src/modules/auto_bill/modify/fill/payments_auto_bill_fill.js
+++ src/modules/auto_bill/modify/fill/payments_auto_bill_fill.js
@@ -57,7 +57,7 @@ angular.module('ocb-payments')
 
         $scope.$on(bdStepStateEvents.FORWARD_MOVE, function (event, actions) {
             var form = $scope.autoBillForm;
-            if (form.$invalid || isNegativeAmount($scope.payment.formData.amountLimit.value) || isZeroAmount($scope.payment.formData.amountLimit.value)) {
+            if (form.$invalid || ($scope.payment.formData.amountLimit  && (isNegativeAmount($scope.payment.formData.amountLimit.value) || isZeroAmount($scope.payment.formData.amountLimit.value)))) {
                formService.dirtyFields(form);
                return false;
            } else {
