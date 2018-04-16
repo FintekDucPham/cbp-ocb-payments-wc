17fcc0ff2ded12ae58a3db195453ba02d6a8f341
jludik <juraj.ludik@sigp.eu>
2018-04-16 14:50:50 +0200

    OCBVNMCP-3701

--- src/modules/auto_bill/modify/verify/payments_auto_bill_verify.js
+++ src/modules/auto_bill/modify/verify/payments_auto_bill_verify.js
@@ -76,7 +76,9 @@ angular.module('ocb-payments')
 
 
             var callParams = $scope.payment.formData;
-            callParams.amountLimit = callParams.amountLimit.value;
+            if(callParams.amountLimit) {
+                callParams.amountLimit = callParams.amountLimit.value;
+            }
             callParams.resourceId = $scope.payment.meta.token.params.resourceId;
             callParams.credentials = $scope.payment.meta.token.model.input.model;
 
